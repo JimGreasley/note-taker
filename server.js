66a96c724cd245b236a053fd1c8a32aa873f9236
Jim Greasley <azjimgreasley@gmail.com>
2020-03-05 09:45:27 -0700

    app now using fs to store notes data

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const uuid    = require("uuid").v4;
 const fs   = require("fs");
 const util   = require("util");
 //const readfile = util.promisify(fs.readFile);
+//const writeFile = util.promisify(fs.writeFile);
 
 const app = express();
 
@@ -24,7 +25,6 @@ app.use(express.static('public'));
 //absolute path: app.use('/static', express.static(path.join(__dirname, 'public')));
 
 //----------------------------------------------------------------
-
 // module.exports = {
 //   async read() {
 //     const notes = await readfile(__dirname + "/db.json", "utf8");
@@ -35,26 +35,42 @@ app.use(express.static('public'));
 
 // Notes array of objects
 // =============================================================
-var notes = [
-  {
-    title: "Note-1",
-    text: "This is test note-1. For debugging purposes.",
-    id: "uuid-123456"
-  },
-  {
-    title: "Note-2",
-    text: "This is test note-2. For debugging purposes.",
-    id: "uuid-123457"
-  }
-];
+//var notes = [];
 
+  // {
+  //   title: "Note-1",
+  //   text: "This is test note-1. For debugging purposes.",
+  //   id: "uuid-123456"
+  // },
+  // {
+  //   title: "Note-2",
+  //   text: "This is test note-2. For debugging purposes.",
+  //   id: "uuid-123457"
+  // }
+// ];
+
+// Initial load of db.json
+//notes.forEach(function(item, index) {
+
+  // fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
+  //   if (err) throw err; 
+  // });
+  //return writeFileAsync(__dirname + "/db.json", "utf8", );
+  
+//});
 
 // Routes
 // ===========================================================
 
 app.get("/api/notes", function(req, res) {
   // Read the `db.json` file and return all saved notes as JSON.
-  res.json(notes);
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    //var notes = [];
+    var notes = JSON.parse(data);
+    res.json(notes);
+  })
 });
 
 
@@ -71,9 +87,21 @@ app.post("/api/notes", function(req, res) {
   // A unique `id` will ned to be assigned to each note when it's saved.
   newNote.id = uuid();        //"uuid-123459";
 
-  notes.push(newNote);
-  
-  res.json(newNote);
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    // create notes array
+    const notes = JSON.parse(data);
+    // save new note in notes array
+    notes.push(newNote);
+    // save updated notes array to file  
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err; 
+      res.json(newNote);
+    });
+
+  //res.json(newNote);
+  })
 });
 
 
@@ -86,15 +114,27 @@ app.delete("/api/notes/:id", function(req, res) {
 
   var noteIdToDelete = req.params.id;
   var noteDeleted = false;
+
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    const notes = JSON.parse(data);
+    //res.json(notes);
   
-  for (var i = 0; i < notes.length && !noteDeleted; i++) {
-    if (notes[i].id === noteIdToDelete) {
-       console.log(notes[i].id);
-       notes.splice(i, 1);
-       noteDeleted = true;
+    for (var i = 0; i < notes.length && !noteDeleted; i++) {
+      if (notes[i].id === noteIdToDelete) {
+        console.log(notes[i].id);
+        notes.splice(i, 1);
+        noteDeleted = true;
+      }
     }
-  }
-  res.json(notes);
+    // save updated notes data to file  
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err; 
+      res.json(notes);
+    })
+    //res.json(notes);
+  })
 });
 
 app.get("/notes", function(req, res) {
@@ -126,6 +166,4 @@ app.get("*", function(req, res) {
 // app.listen(PORT, function() {
 //   console.log("App listening on PORT " + PORT);
 // });
-app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`))
-
-
+app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
