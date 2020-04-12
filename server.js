b79cdf29826e0e903b6affcb4d763fbdb85c08e7
Jim Greasley <azjimgreasley@gmail.com>
2020-04-12 15:10:17 -0700

    sync file names and minor clean-up

--- server.js
+++ server.js
@@ -148,22 +148,5 @@ app.get("*", function(req, res) {
 });
 
 
-
-// ??
-// app.use(function (req, res, next) {
-//   res.status(404).send("Sorry can't find that!")
-// });
-
-// ???
-// app.use(function (err, req, res, next) {
-//   console.error(err.stack)
-//   res.status(500).send('Something broke!')
-// });
-
-
 // Listener
-// ===========================================================
-// app.listen(PORT, function() {
-//   console.log("App listening on PORT " + PORT);
-// });
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
